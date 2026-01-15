2b075981f9271dce02216e3da74cddf63aacd85b
Kseniya <baharevaxen@yandex.ru>
2026-01-15 22:00:48 +0300

    correcting User 2

--- users/models.py
+++ users/models.py
@@ -8,6 +8,22 @@ class User(AbstractUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_set',
+        blank=True,
+        help_text='Группы, к которым принадлежит пользователь.',
+        verbose_name='Группы'
+    )
+
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_permissions_set',
+        blank=True,
+        help_text='Разрешения, которые у пользователя.',
+        verbose_name='Разрешения'
+    )
+
     def __str__(self):
         return self.email
 
