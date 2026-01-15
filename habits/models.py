06e20155872c02c59bd9c9c9b2b5df61b74ab149
Kseniya <baharevaxen@yandex.ru>
2026-01-15 21:25:32 +0300

    correcting models 2

--- habits/models.py
+++ habits/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
 from users.models import User
-from .models import PleasantHabit
-from .models import Place
+# from .models import PleasantHabit
+# from .models import Place
 
 
 class Habit(models.Model):
     """ Модель Полезная привычка"""
     name = models.CharField(max_length=150, unique=True, help_text='Название привычки')
     user = models.ForeignKey(User, related_name='habits', on_delete=models.CASCADE,help_text='Пользователь')
-    place = models.ForeignKey(Place, on_delete=models.CASCADE, help_text='Место выполнения привычки')
+    place = models.ForeignKey('Place', on_delete=models.CASCADE, help_text='Место выполнения привычки')
     time = models.TimeField(help_text='Время выполнения привычки')
     action = models.CharField(max_length=250, help_text='Действие связанное с привычкой')
-    pleasant_habit = models.ForeignKey(PleasantHabit, null=True, blank=True, on_delete=models.SET_NULL, related_name='pleasant_habits', help_text='Приятная привычка')
+    pleasant_habit = models.ForeignKey('PleasantHabit', null=True, blank=True, on_delete=models.SET_NULL, related_name='pleasant_habits', help_text='Приятная привычка')
     periodic = models.PositiveIntegerField(default=1, help_text='Периодичность выполнения привычки в днях')
     reward = models.CharField(max_length=250, null=True, blank=True, help_text='Вознаграждение за привычку')
     duration = models.PositiveIntegerField(help_text='Время на выполнение в секундах')
