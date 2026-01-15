d395dd830356c5e102e4a3921efc31ad1aa1cfe6
Kseniya <baharevaxen@yandex.ru>
2026-01-15 21:34:08 +0300

    creating User

--- users/models.py
+++ users/models.py
@@ -3,4 +3,12 @@ from django.db import models
 
 class User(AbstractUser):
     """ модель Пользователей"""
-    name =
+    name = models.CharField(max_length=150, null=True, blank=True, help_text='Имя Пользователя')
+
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        plural_verbose_name = 'Пользователи'
