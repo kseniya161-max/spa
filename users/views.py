5943504ba7c7ce94dcf76e4a2a5ecf8851500fa0
Kseniya <baharevaxen@yandex.ru>
2026-01-16 20:02:56 +0300

    creating iew for registration

--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
 
-# Create your views here.
+from users.forms import CustomUsersCreationForm
+
+
+class RegistrationView(View):
+    def get (self, request):
+        form = CustomUsersCreationForm()
+        return render(request, 'users/registration.html', {'form': form})
+
+
+    def post(self, request):
+        form = CustomUsersCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        return render (request, 'users/registration.html', {'form': form} )
+
+
