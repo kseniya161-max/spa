cc3997abf9f2d4a023084836755fe01c2efe98d8
Kseniya <baharevaxen@yandex.ru>
2026-01-17 19:04:00 +0300

    urls for habit

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('habits.urls')),
     path('admin/', admin.site.urls),
+    path('', include('habits.urls')),
     path('users/', include('users.urls')),
 ]
